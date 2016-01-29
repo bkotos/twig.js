4f61feef7ae9626306cf5dfab7cca0474062395b
bkotos <bkotos@dattobackup.com>
2016-01-29 17:14:21 -0500

    Unit test for the "source" function implementation.

--- test/test.functions.js
+++ test/test.functions.js
@@ -311,7 +311,17 @@ describe("Twig.js Functions ->", function() {
             it("should support the 'max' function", function() {
                 twig({data: '{{ max([2, 1, 3, 5, 4]) }}'}).render().should.equal('5');
                 twig({data: '{{ max(2, 1, 3, 5, 4) }}'}).render().should.equal('5');
-                twig({data: '{{ max({2:"two", 1:"one", 3:"three", 5:"five", 4:"four"}) }}'}).render().should.equal('two');
+                twig({data: '{{ source("") }}'}).render().should.equal('two');
+            });
+        });
+
+        describe("source ->", function() {
+            it("should load the non-compiled template source", function() {
+                twig({data: '{{ source("templates/source.twig") }}'})
+                  .render()
+                  .should
+                  .equal('{% if isUserNew == true %}\n  Hello {{ name }}\n{% else %}\n  Welcome back {{ name }}\n{% endif %}\n')
+                ;
             });
         });
     });
